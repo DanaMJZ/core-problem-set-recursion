ebffaf153d294e0196925867fe26c8203334a2ae
Dana Al Shaar <da.alshaar@gmail.com>
2025-05-05 05:59:07 -0700

    part1,2 codes

--- part-1.py
+++ part-1.py
@@ -1,20 +1,31 @@
-# There are comments with the names of
-# the required functions to build.
-# Please paste your solution underneath
-# the appropriate comment.
-
 # factorial
-
+def factorial(n):
+    if n < 0:
+        raise ValueError
+    if n == 0:
+        return 1
+    return n * factorial(n - 1)
 
 
 # reverse
-
+def reverse(text):
+    if len(text) <= 1:
+        return text
+    return reverse(text[1:]) + text
 
 
 # bunny
-
+def bunny(count):
+    if count == 0:
+        return 0
+    return 2 + bunny(count - 1)
 
 
 # is_nested_parens
-
+def is_nested_parens(parens):
+    if not parens:
+        return True
+    return (parens[0] == '(' and 
+            parens[-1] == ')' and 
+            is_nested_parens(parens[1:-1]))
 
--- part-2.py
+++ part-2.py
@@ -1,16 +1,31 @@
-# There are comments with the names of
-# the required functions to build.
-# Please paste your solution underneath
-# the appropriate comment.
-
 # search
-
+def search(array, query):
+    if not array:
+        return False
+    if array[0] == query:
+        return True
+    return search(array[1:], query)
 
 
 # is_palindrome
-
+def is_palindrome(text):
+    if len(text) <= 1:
+        return True
+    if text[0] != text[-1]:
+        return False
+    return is_palindrome(text[1:-1])
 
 
 # digit_match
+def digit_match(apples, oranges):
+    def helper(a, o):
+        if a == 0 or o == 0:
+            return 0
+        match = 1 if a % 10 == o % 10 else 0
+        return match + helper(a // 10, o // 10)
+
+    if apples == 0 and oranges == 0:
+        return 1  
+    return helper(apples, oranges)
 
 
